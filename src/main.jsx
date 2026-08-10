1a4de589b69cb1c07b02071d078e0ffeac032c76
vamsi0143 <126938702+vamsi0143@users.noreply.github.com>
2026-08-10 17:17:10 +0530

    Change BrowserRouter to HashRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import "./index.css";
@@ -13,10 +13,10 @@ import { store } from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <Toaster position="top-right" />
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
